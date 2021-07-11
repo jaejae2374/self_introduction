83faf19a8111741abff2f8e7d4ff3b7d2c38559f
jaehyunlee <jaejae2374@snu.ac.kr>
2021-07-12 01:40:49 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/<int:pk>/', views.PostDetail.as_view()),
-    path('/', views.PostList.as_view()),
+    path('category/<str:slug>/', views.category_page),
+    path('<int:pk>/', views.PostDetail.as_view()),
+    path('', views.PostList.as_view()),
 
 ]
