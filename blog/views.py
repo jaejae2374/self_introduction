f38c4b6c2f8af354ec2594c339c520a1887e3af2
jaehyunlee <jaejae2374@snu.ac.kr>
2021-07-21 02:07:56 +0900

    포스트 수정 기능 추가하기

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,11 @@
 from typing import List
+
+from django.contrib.auth.models import User
 from .models import Post, Category, Tag
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.shortcuts import render, redirect
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.core.exceptions import PermissionDenied
 
 class PostList(ListView):
     model = Post
@@ -14,17 +17,34 @@ class PostList(ListView):
         context['no_category_post_count'] = Post.objects.filter(category=None).count()
         return context
 
-class PostCreate(LoginRequiredMixin, CreateView):
+class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
     model = Post
     fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category']
 
+    def test_func(self):
+        return self.request.user.is_superuser or self.request.user.is_staff
+
     def form_valid(self, form):
         current_user = self.request.user
-        if current_user.is_authenticated :
+        if current_user.is_authenticated and (current_user.is_staff or current_user.is_superuser):
             form.instance.author = current_user
             return super(PostCreate, self).form_valid(form)
         else :
             return redirect('/blog/')
+
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    template_name = 'blog/post_update_form.html'
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and (request.user == self.get_object().author) :
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else :
+            raise PermissionDenied
+
 
 def tag_page(request, slug):
     tag = Tag.objects.get(slug=slug)
