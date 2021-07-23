1bf817fcfe5adcaaf560e05562e3540683520d71
jaehyunlee <jaejae2374@snu.ac.kr>
2021-07-23 19:50:48 +0900

    검색 기능 구현하기

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from typing import List
 
 from django.contrib.auth.models import User
+from django.db.models.expressions import F
 from .models import Post, Category, Tag, Comment
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.shortcuts import render, redirect
@@ -9,6 +10,7 @@ from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
 from .forms import CommentForm
 from django.shortcuts import get_object_or_404
+from django.db.models import Q
 
 class PostList(ListView):
     model = Post
@@ -177,3 +179,20 @@ def delete_comment(request, pk):
 
     else : 
         raise PermissionDenied
+
+class PostSearch(PostList):
+    paginate_by = None
+
+    def get_queryset(self):
+        q = self.kwargs['q']
+        post_list = Post.objects.filter(
+            Q(title__contains=q) | Q(tags__name__contains=q)
+        ).distinct()
+        return post_list
+    
+    def get_context_data(self, **kwargs):
+        context = super(PostSearch, self).get_context_data()
+        q = self.kwargs['q']
+        context['search_info'] = f'Search: {q} ({self.get_queryset().count()})'
+
+        return context
